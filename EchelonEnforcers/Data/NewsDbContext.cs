7eac1e02d4287c31386a92e5b00b629ffc05ebf4
DanWin85 <dan.wingate85@gmail.com>
2023-11-13 23:55:41 +1300

    fixed a heap of mistakes and double ups

--- EchelonEnforcers/Data/NewsDbContext.cs
+++ EchelonEnforcers/Data/NewsDbContext.cs
@@ -1,13 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
-<<<<<<< HEAD
-using EchelonEnforcers.Models;
-=======
-using EchelonEnforcers.Models.Domain;
->>>>>>> a22c04f05efee47888f5330dceafe892c783e142
+﻿using Microsoft.EntityFrameworkCore;
+
 
 namespace EchelonEnforcers.Data
 {
@@ -18,12 +10,10 @@ namespace EchelonEnforcers.Data
         {
         }
 
-<<<<<<< HEAD
         public DbSet<EchelonEnforcers.Models.NewsModel> NewsModel { get; set; } = default!;
-=======
-        public DbSet<EchelonEnforcers.Models.Domain.News> News { get; set; } = default!;
 
-        public DbSet<Tag> Tags { get; set; }
->>>>>>> a22c04f05efee47888f5330dceafe892c783e142
+        public DbSet<EchelonEnforcers.Models.NewsModel> News { get; set; } = default!;
+
+       
     }
 }
